c8995ecf0054cac8feb8b8be906df760f1aaf3cc
Eugene Daragan <eugene.daragan@pandadoc.com>
2023-09-14 09:35:28 +0200

    refactor, pure code

---
+++ src/index.ts
@@ -0,0 +1,275 @@
+type DragSourceSymbol = string | symbol;
+
+export type DragSourceType<Data> = DragSourceSymbol & { __data: Data };
+
+export type DragSourceConfig<T> = {
+  type: DragSourceType<T>;
+  data: T;
+  onDragStart: (params: {
+    event: MouseEvent;
+    startCoords: StartCoords;
+  }) => boolean | undefined | void;
+  onDragEnd: (params: {
+    event: MouseEvent;
+    startCoords: StartCoords;
+    dropTargets: Map<Element, DropTarget<any>>;
+  }) => void;
+  onDragMove: (params: {
+    event: MouseEvent;
+    startCoords: StartCoords;
+    dropTargets: Map<Element, DropTarget<any>>;
+  }) => void;
+  mouseConfig?: {
+    mouseDown?: (element: HTMLElement, handler: (event: MouseEvent) => void) => () => void;
+    mouseMove?: (document: Document, handler: (event: MouseEvent) => void) => () => void;
+    mouseUp?: (document: Document, handler: (event: MouseEvent) => void) => () => void;
+  };
+};
+
+export type DropHandlerProps<T> = T extends DragSourceType<infer Data>
+  ? { type: DragSourceSymbol; data: Data; event: MouseEvent }
+  : never;
+
+export type DropHandler<T> = (props: DropHandlerProps<T>) => void;
+
+export type DropTargetConfig<T> = {
+  sourceTypes: Array<T>;
+  data?: any;
+  onDragIn?: DropHandler<T>;
+  onDragOut?: DropHandler<T>;
+  onDragMove?: DropHandler<T>;
+  onDrop?: DropHandler<T>;
+};
+
+type StartCoords = {
+  x: number;
+  y: number;
+  elementX: number;
+  elementY: number;
+  offsetX: number;
+  offsetY: number;
+};
+
+export const DROP_TARGET_ATTRIBUTE = "data-drop-target";
+
+export const DRAGGABLE_ATTRIBUTE = "data-draggable";
+
+export function dragSourceType<Data>(type: DragSourceSymbol): DragSourceType<Data> {
+  return type as DragSourceType<Data>;
+}
+
+function defaultMouseDownHandler(element: HTMLElement, handler: (event: MouseEvent) => void) {
+  element.addEventListener("pointerdown", handler);
+
+  return () => {
+    element.removeEventListener("pointerdown", handler);
+  };
+}
+
+function defaultMouseMoveHandler(document: Document, handler: (event: MouseEvent) => void) {
+  document.addEventListener("pointermove", handler);
+
+  return () => {
+    document.removeEventListener("pointermove", handler);
+  };
+}
+
+function defaultMouseUpHandler(document: Document, handler: (event: MouseEvent) => void) {
+  document.addEventListener("pointerup", handler);
+
+  return () => {
+    document.removeEventListener("pointerup", handler);
+  };
+}
+
+const dropTargets = new WeakMap<Element, DropTarget<any>>();
+
+export class DragSource<T> {
+  private dragElement: HTMLElement | null = null;
+
+  private dragStartTriggered = false;
+
+  private dragStartEvent: MouseEvent | null = null;
+
+  private currentDropTargets = new Map<Element, DropTarget<any>>();
+
+  private startCoords: StartCoords | null = null;
+
+  private mouseEventsDestructor: (() => void) | null = null;
+
+  constructor(public config: DragSourceConfig<T>) {}
+
+  private mouseMoveHandler = (event: MouseEvent) => {
+    const { onDragStart, onDragMove, data, type } = this.config!;
+
+    if (!this.dragStartTriggered) {
+      const dragStartEvent = this.dragStartEvent!;
+
+      const { left, top } = this.dragElement!.getBoundingClientRect();
+
+      this.startCoords = {
+        x: dragStartEvent.x,
+        y: dragStartEvent.y,
+        elementX: left,
+        elementY: top,
+        offsetX: dragStartEvent.x - left,
+        offsetY: dragStartEvent.y - top,
+      };
+
+      if (onDragStart?.({ event: dragStartEvent, startCoords: this.startCoords })) {
+        this.cleanup();
+
+        return;
+      }
+
+      this.dragStartTriggered = true;
+    }
+
+    const targetElementsList = document.elementsFromPoint(event.x, event.y);
+
+    const targetElements = targetElementsList ? Array.from(targetElementsList) : [];
+
+    const newTargets = new Map<Element, DropTarget<any>>();
+
+    targetElements.forEach((element) => {
+      const isDropTarget = element.getAttribute(DROP_TARGET_ATTRIBUTE);
+
+      if (!isDropTarget || isDropTarget === "false") {
+        return;
+      }
+
+      const dropTarget = dropTargets.get(element);
+
+      if (dropTarget) {
+        newTargets.set(element, dropTarget);
+
+        if (!this.currentDropTargets.has(element)) {
+          dropTarget.config.onDragIn?.({ type, data, event });
+        }
+      }
+    });
+
+    this.currentDropTargets.forEach((dropTarget, element) => {
+      if (!newTargets.has(element)) {
+        dropTarget.config.onDragOut?.({ type, data, event });
+      }
+    });
+
+    this.currentDropTargets = newTargets;
+
+    newTargets.forEach((dropTarget) => {
+      dropTarget.config.onDragMove?.({ type, data, event });
+    });
+
+    onDragMove?.({
+      event,
+      startCoords: this.startCoords!,
+      dropTargets: this.currentDropTargets,
+    });
+  };
+
+  private mouseUpHandler = (event: MouseEvent) => {
+    if (this.dragStartTriggered) {
+      const { data, type, onDragEnd } = this.config!;
+
+      this.currentDropTargets.forEach((dropTarget) => {
+        dropTarget.config.onDrop?.({ type, data, event });
+      });
+
+      onDragEnd?.({
+        event,
+        dropTargets: this.currentDropTargets,
+        startCoords: this.startCoords!,
+      });
+    }
+
+    this.cleanup();
+  };
+
+  private mouseDownHandler = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+
+    const dragElement = target.closest(`[${DRAGGABLE_ATTRIBUTE}]`);
+
+    if (!dragElement) {
+      return;
+    }
+
+    if (dragElement.getAttribute(DRAGGABLE_ATTRIBUTE) === "false") {
+      return;
+    }
+
+    this.dragStartEvent = event;
+    this.dragElement = dragElement as HTMLElement;
+
+    const mouseMoveHandler = this.config.mouseConfig?.mouseMove ?? defaultMouseMoveHandler;
+    const mouseUpHandler = this.config.mouseConfig?.mouseUp ?? defaultMouseUpHandler;
+
+    const mouseMoveDestructor = mouseMoveHandler(document, this.mouseMoveHandler);
+    const mouseUpDestructor = mouseUpHandler(document, this.mouseUpHandler);
+
+    this.mouseEventsDestructor = () => {
+      mouseMoveDestructor();
+      mouseUpDestructor();
+    };
+  };
+
+  private cleanup = () => {
+    this.mouseEventsDestructor?.();
+
+    this.startCoords = null;
+    this.dragElement = null;
+    this.dragStartEvent = null;
+    this.dragStartTriggered = false;
+
+    this.currentDropTargets.clear();
+  };
+
+  public setConfig = (config: DragSourceConfig<T>) => {
+    this.config = config;
+  }
+
+  public apply = (element: HTMLElement): (() => void) => {
+    const mouseDownHandler = this.config.mouseConfig?.mouseDown ?? defaultMouseDownHandler;
+
+    const mouseDownDestructor = mouseDownHandler(element, this.mouseDownHandler);
+
+    element.setAttribute(DRAGGABLE_ATTRIBUTE, "true");
+
+    return () => {
+      this.cleanup();
+
+      mouseDownDestructor();
+
+      element.removeAttribute(DRAGGABLE_ATTRIBUTE);
+    };
+  };
+}
+
+export class DropTarget<T> {
+  constructor(public config: DropTargetConfig<T>) {}
+
+  public setConfig = (config: DropTargetConfig<T>) {
+    this.config = config;
+  }
+
+  public apply = (element: HTMLElement) => {
+    element.setAttribute(DROP_TARGET_ATTRIBUTE, "true");
+
+    dropTargets.set(element, this);
+
+    return () => {
+      element.removeAttribute(DROP_TARGET_ATTRIBUTE);
+
+      dropTargets.delete(element);
+    };
+  };
+}
+
+export function createDragSource<T>(config: DragSourceConfig<T>) {
+  return new DragSource(config);
+}
+
+export function createDropTarget<T>(config: DropTargetConfig<T>) {
+  return new DropTarget(config);
+}
--- src/react.tsx
+++ src/react.tsx
@@ -1,33 +1,44 @@
-import { PropsWithChildren } from "react";
-import type { DragSourceConfig, IDragSourceHandler, IDropTargetHandler } from "./index.class";
+import { PropsWithChildren, useEffect, useMemo, useState } from "react";
+import { DragSourceConfig, DropTargetConfig, createDragSource, createDropTarget } from "./index";
 import React, { useRef, useCallback } from "react";
 
 type DragSourceProps = PropsWithChildren<{
-  handler: IDragSourceHandler<any>;
+  config: DragSourceConfig<any>;
 }>;
 
 type DropTargetProps = PropsWithChildren<{
-  handler: IDropTargetHandler<any>;
-}>
+  config: DropTargetConfig<any>;
+}>;
 
-export const DragSource = React.forwardRef(function DragSource(props: DragSourceProps, componentRef) {
-  const { children, handler } = props;
+export const DragSource = React.forwardRef(function DragSource(
+  props: DragSourceProps,
+  componentRef: any
+) {
+  const { children, config } = props;
 
   const child = React.Children.only(children) as any;
 
   const originalRef = child.ref;
 
-  const destructorRef = useRef<(() => void) | null>(null);
+  const [element, setElement] = useState<HTMLElement | null>(null);
+
+  const dragSource = useMemo(() => createDragSource(config), []);
+
+  useEffect(() => {
+    if (element) {
+      const destructor = dragSource.apply(element);
+
+      return destructor;
+    }
+  }, [element]);
+
+  useEffect(() => {
+    dragSource.setConfig(config);
+  }, [config]);
 
   const childRef = useCallback(
     (element: HTMLElement) => {
-      if (destructorRef.current) {
-        destructorRef.current();
-      }
-
-      if (element) {
-        destructorRef.current = handler.startListening(element);
-      }
+      setElement(element);
 
       if (typeof originalRef === "function") {
         originalRef(element);
@@ -47,24 +58,35 @@ export const DragSource = React.forwardRef(function DragSource(props: DragSource
   return React.cloneElement(child, { ref: childRef });
 });
 
-export const DropTarget = React.forwardRef(function DropTarget(props: DragSourceProps, componentRef) {
-  const { children, handler } = props;
+export const DropTarget = React.forwardRef(function DropTarget(
+  props: DropTargetProps,
+  componentRef: any
+) {
+  const { children, config } = props;
 
   const child = React.Children.only(children) as any;
 
   const originalRef = child.ref;
 
-  const destructorRef = useRef<(() => void) | null>(null);
+  const [element, setElement] = useState<HTMLElement | null>(null);
+
+  const dragSource = useMemo(() => createDropTarget(config), []);
+
+  useEffect(() => {
+    if (element) {
+      const destructor = dragSource.apply(element);
+
+      return destructor;
+    }
+  }, [element]);
+
+  useEffect(() => {
+    dragSource.setConfig(config);
+  }, [config]);
 
   const childRef = useCallback(
     (element: HTMLElement) => {
-      if (destructorRef.current) {
-        destructorRef.current();
-      }
-
-      if (element) {
-        destructorRef.current = handler.startListening(element);
-      }
+      setElement(element);
 
       if (typeof originalRef === "function") {
         originalRef(element);
@@ -82,56 +104,4 @@ export const DropTarget = React.forwardRef(function DropTarget(props: DragSource
   );
 
   return React.cloneElement(child, { ref: childRef });
-})
-
-class DragOverlay extends React.PureComponent<
-    { className: string; style: any },
-    DragOverlayState
-  > {
-    state: DragOverlayState = {
-      mouseEvent: null,
-      renderDragComponent: null,
-    };
-
-    componentDidMount(): void {
-      overlayComponentInstance = this;
-    }
-
-    componentWillUnmount(): void {
-      overlayComponentInstance = null;
-    }
-
-    setMouseEvent = (event: MouseEvent | null) => {
-      this.setState({ mouseEvent: event });
-    };
-
-    setRenderDragComponent = (renderDragComponent: any) => {
-      this.setState({ renderDragComponent });
-    };
-
-    render() {
-      const { className, style } = this.props;
-      const { mouseEvent, renderDragComponent } = this.state;
-
-      if (!mouseEvent) {
-        return null;
-      }
-
-      const positionWrapperStyle = {
-        position: "absolute",
-        transform: `translateX(${mouseEvent.x}px) translateY(${mouseEvent.y}px)`,
-      } as const;
-
-      return (
-        <div className={className} style={style}>
-          <div style={positionWrapperStyle}>
-            {renderDragComponent?.({
-              event: mouseEvent,
-              dropTargets,
-              ...startCoords!,
-            })}
-          </div>
-        </div>
-      );
-    }
-  }
+});
